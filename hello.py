c4d0e90f3abba7f466e4aad2198e673a23d8241a
gabriellenoelle <88753098+gabriellenoelle@users.noreply.github.com>
2022-03-26 12:11:20 -0400

    added variable examples

--- hello.py
+++ hello.py
@@ -1,1 +1,23 @@
 print("Hello, python!")
+
+# indentations are important in python
+# you will get an error if it's not indented properly
+
+if 5 > 2:
+    print("Five is greater than two!") # correct - this prints the string
+
+# python has no command for declaring a variable
+x = 5
+y = "Hello, python!"
+
+print(x)
+print(y)
+
+# variables can be ay "type" and can even change type...
+# ...after they are set
+
+x = 4
+x = "Sally"
+print(x) # will print "Sally" even though x has been defined 3 times
+
+
